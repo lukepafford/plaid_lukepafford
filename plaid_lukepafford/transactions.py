264b246bcfcac5fc9edab9915ed357de2ebca75f
Luke Pafford <lukepafford@gmail.com>
2020-06-20 01:21:18 -0700

    Ensure cache directory exists before writing to it

--- plaid_lukepafford/transactions.py
+++ plaid_lukepafford/transactions.py
@@ -107,5 +107,6 @@ class ChaseTransactions:
         self._write()
 
     def _write(self) -> None:
+        self.cache.mkdir(parents=True, exist_ok=True)
         with open(self.cache, "w") as f:
             json.dump(self.transactions, f)
